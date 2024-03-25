412fb3d858786af11e3194d1c9397ec8d327d670
marina.pyanykh@inbox.ru <marina.pyanykh@inbox.ru>
2024-03-25 23:41:43 +0300

    Initial commit

---
+++ src/main/java/com/example/appcalculationpayvacation/AppCalculationPayVacationApplication.java
@@ -0,0 +1,13 @@
+package com.example.appcalculationpayvacation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCalculationPayVacationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCalculationPayVacationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appcalculationpayvacation/AppCalculationPayVacationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appcalculationpayvacation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCalculationPayVacationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
